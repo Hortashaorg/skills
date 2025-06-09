81d5e56d397779ff5f59f974f052f1c476e6ad24
Hortasha <eidemartin_303@hotmail.com>
2025-06-09 12:08:03 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+
+export default defineConfig({
+  plugins: [solid()],
+  server: {
+    port: 5432,
+    host: "0.0.0.0",
+  },
+  build: {
+    outDir: "dist",
+  },
+  esbuild: {
+    target: "esnext",
+  },
+});
