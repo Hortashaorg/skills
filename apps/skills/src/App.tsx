b673cb2ebfc42d609dac515b5c42b76fef041fcf
Hortasha <eidemartin_303@hotmail.com>
2025-06-18 17:18:05 +0000

    able to bundle ts files without building first

--- apps/skills/src/App.tsx
+++ apps/skills/src/App.tsx
@@ -1,6 +1,8 @@
+import { test } from "@package/database";
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
+  console.log(test())
   return (
     <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
   );
