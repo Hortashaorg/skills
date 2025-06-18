5d636d24b9d16657375d6e41d140970d4827082e
Hortasha <eidemartin_303@hotmail.com>
2025-06-18 14:56:54 +0000

    fix biome for organizing imports on save

--- packages/database/schema.ts
+++ packages/database/schema.ts
@@ -1,12 +1,20 @@
-import { ANYONE_CAN_DO_ANYTHING, type Row, definePermissions } from "@rocicorp/zero";
-import { schema, type Schema } from "./zero-schema.gen";
+import {
+    ANYONE_CAN_DO_ANYTHING,
+    definePermissions,
+    type Row,
+} from "@rocicorp/zero";
+
+import { type Schema, schema } from "./zero-schema.gen";
 
 export { schema, type Schema };
 
 export type Account = Row<Schema["tables"]["account"]>;
 
-export const permissions = definePermissions<{}, Schema>(schema, () => {
-  return {
-    	account: ANYONE_CAN_DO_ANYTHING,
-  }
-});
+export const permissions = definePermissions<Record<string, never>, Schema>(
+	schema,
+	() => {
+		return {
+			account: ANYONE_CAN_DO_ANYTHING,
+		};
+	},
+);
