aed9fb02654615eeaf9b47150c3aeaf212ade0e9
Hortasha <eidemartin_303@hotmail.com>
2025-06-21 19:21:59 +0000

    removed mutation

--- packages/database/client.ts
+++ packages/database/client.ts
@@ -1,4 +1,3 @@
 export { Zero } from "@rocicorp/zero";
 export { createQuery, createZero, useQuery } from "@rocicorp/zero/solid";
-export type { Mutators } from "./mutators.ts";
 export { type Schema, schema } from "./schema.ts";
