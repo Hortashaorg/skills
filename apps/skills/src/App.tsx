6dae4a6f3aa7669a5909e7947a048fb93dc1c38e
Hortasha <eidemartin_303@hotmail.com>
2025-06-21 10:26:21 +0000

    formatter and linter is now correct

--- apps/skills/src/App.tsx
+++ apps/skills/src/App.tsx
@@ -2,12 +2,12 @@ import type { Component } from "solid-js";
 import { Button } from "@/components/ui/button.tsx";
 
 const App: Component = () => {
-	return (
-		<div>
-			<p class="p-4 text-green-500 text-center">Hello tailwind!</p>
-			<Button>Hello world</Button>
-		</div>
-	);
+    return (
+        <div>
+            <p class="p-4 text-green-500 text-center">Hello tailwind!</p>
+            <Button>Hello world</Button>
+        </div>
+    );
 };
 
 export default App;
