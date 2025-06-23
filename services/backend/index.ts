71cc9cdf104874810e1851fed10b8fe11e1470d5
Hortasha <eidemartin_303@hotmail.com>
2025-06-23 16:54:20 +0000

    working hono app

--- services/backend/index.ts
+++ services/backend/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world!");
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono().basePath("/api");
+
+app.get("/login", (c) => c.text("Login"));
+
+serve({
+    fetch: app.fetch,
+    port: 4000,
+});
