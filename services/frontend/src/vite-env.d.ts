7d90e0825ae79d292f0e15772d935fcdf66620c4
Hortasha <eidemartin_303@hotmail.com>
2025-07-05 13:04:46 +0000

    fix packages

--- services/frontend/src/vite-env.d.ts
+++ services/frontend/src/vite-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-// biome-ignore lint/correctness/noUnusedVariables: Do not need to be used.
+// biome-ignore lint/correctness/noUnusedVariables: Not used
 interface ImportMetaEnv {
 	readonly VITE_URL: string;
 }
