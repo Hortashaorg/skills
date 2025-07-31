7b473d46c9bf1a5b126a5b84c538213056e04af0
Hortasha <eidemartin_303@hotmail.com>
2025-07-31 16:40:53 +0000

    add navbar

--- services/frontend/src/layout/Layout.tsx
+++ services/frontend/src/layout/Layout.tsx
@@ -1,9 +1,27 @@
 import type { ParentComponent } from "solid-js";
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuIcon,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 
 export const Layout: ParentComponent = ({ children }) => {
 	return (
 		<div>
-			Layout
+			<NavigationMenu>
+				<NavigationMenuItem>
+					<NavigationMenuTrigger>
+						Item one
+						<NavigationMenuIcon />
+					</NavigationMenuTrigger>
+					<NavigationMenuContent>
+						<NavigationMenuLink>Link</NavigationMenuLink>
+					</NavigationMenuContent>
+				</NavigationMenuItem>
+			</NavigationMenu>
 			{children}
 		</div>
 	);
