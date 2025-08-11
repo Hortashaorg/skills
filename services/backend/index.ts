7fbf316aecfa88004c1802213cb67671ccb6cfe7
Hortasha <eidemartin_303@hotmail.com>
2025-08-11 09:27:45 +0000

    login flow

--- services/backend/index.ts
+++ services/backend/index.ts
@@ -3,6 +3,8 @@ import { throwError } from "@package/common";
 import { Hono } from "hono";
 import { getCookie, setCookie } from "hono/cookie";
 import { cors } from "hono/cors";
+import { decode } from "hono/jwt";
+import { ensureUser } from "./ensure-user.ts";
 import { environment } from "./environment.ts";
 import { handlePush } from "./push.ts";
 
@@ -15,6 +17,57 @@ app.use(
 		credentials: true,
 	}),
 );
+
+app.post("/login", async (c) => {
+	const { code } = await c.req.json();
+
+	if (!code) throwError("Code not provided");
+
+	const params = new URLSearchParams({
+		client_id: environment.CLIENT_ID,
+		client_secret: environment.CLIENT_SECRET,
+		redirect_uri: "http://localhost:4321/auth/callback",
+		scope: "email",
+		grant_type: "authorization_code",
+		access_type: "offline",
+		prompt: "consent",
+		code,
+	});
+
+	const res = await fetch(`https://oauth2.googleapis.com/token`, {
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		method: "POST",
+		body: params,
+	});
+	if (res.ok) {
+		const result = await res.json();
+
+		const { payload } = decode(result.id_token);
+
+		const email = (payload.email as string) ?? throwError("No email in claim");
+		const user = await ensureUser(email);
+		console.log("login", user);
+
+		// biome-ignore lint/suspicious/noExplicitAny: Context type does not fit here.
+		setCookie(c as any, "refresh_token", result.refresh_token, {
+			maxAge: 6 * 30 * 24 * 60 * 60, // 6 months in seconds
+			httpOnly: true,
+			secure: environment.NODE_ENV !== "local",
+			sameSite: "lax",
+		});
+
+		return c.json({
+			access_token: result.access_token,
+		});
+	} else {
+		console.log(res.statusText);
+		res.statusText;
+	}
+
+	return c.text("Login");
+});
 
 app.post("/refresh", async (c) => {
 	const refreshToken = getCookie(c, "refresh_token");
@@ -45,50 +98,11 @@ app.post("/refresh", async (c) => {
 	if (res.ok) {
 		const result = await res.json();
 
-		return c.json({
-			access_token: result.access_token,
-		});
-	} else {
-		console.log(res.statusText);
-		res.statusText;
-	}
+		const { payload } = decode(result.id_token);
 
-	return c.text("Login");
-});
-
-app.post("/login", async (c) => {
-	const { code } = await c.req.json();
-
-	if (!code) throwError("Code not provided");
-
-	const params = new URLSearchParams({
-		client_id: environment.CLIENT_ID,
-		client_secret: environment.CLIENT_SECRET,
-		redirect_uri: "http://localhost:4321/auth/callback",
-		scope: "email",
-		grant_type: "authorization_code",
-		access_type: "offline",
-		prompt: "consent",
-		code,
-	});
-
-	const res = await fetch(`https://oauth2.googleapis.com/token`, {
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded",
-		},
-		method: "POST",
-		body: params,
-	});
-	if (res.ok) {
-		const result = await res.json();
-
-		// biome-ignore lint/suspicious/noExplicitAny: Context type does not fit here.
-		setCookie(c as any, "refresh_token", result.refresh_token, {
-			maxAge: 6 * 30 * 24 * 60 * 60, // 6 months in seconds
-			httpOnly: true,
-			secure: environment.NODE_ENV !== "local",
-			sameSite: "lax",
-		});
+		const email = (payload.email as string) ?? throwError("No email in claim");
+		const user = await ensureUser(email);
+		console.log("refresh", user);
 
 		return c.json({
 			access_token: result.access_token,
