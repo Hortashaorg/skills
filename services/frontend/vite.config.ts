c260d1f804e13ac74848438144ea35537af1c0e3
Hortasha <eidemartin_303@hotmail.com>
2025-11-03 13:53:59 +0000

    fix setup for testing

--- services/frontend/vite.config.ts
+++ services/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import { playwright } from '@vitest/browser-playwright';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -22,25 +23,27 @@ export default defineConfig({
 			"@sb": resolve(__dirname, "./.storybook"),
 		},
 	},
-	test: {
-		projects: [
-			{
-				extends: true,
-				plugins: [
-					// The plugin will run tests for the stories defined in your Storybook config
-					// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-					storybookTest({
-						configDir: join(__dirname, ".storybook"),
-					}),
-				],
-				test: {
-					name: "storybook",
-					browser: {
-						enabled: true,
-						headless: true,
-					},
-				},
-			},
-		],
-	},
+    test: {
+        projects: [{
+            extends: true,
+            plugins: [
+                // The plugin will run tests for the stories defined in your Storybook config
+                // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+                storybookTest({
+                    configDir: join(__dirname, '.storybook')
+                })
+            ],
+            test: {
+                name: 'storybook',
+                browser: {
+                    enabled: true,
+                    headless: true,
+                    provider: playwright(),
+                    instances: [{
+                        browser: 'chromium'
+                    }]
+                },
+            }
+        }]
+  }
 });
