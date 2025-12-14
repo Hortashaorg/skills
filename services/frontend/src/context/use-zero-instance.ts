1cd97b093219c718c636e9e72c00f006f67ec260
Hortasha <eidemartin_303@hotmail.com>
2025-12-14 19:23:16 +0000

    remove dead code

--- services/frontend/src/context/use-zero-instance.ts
+++
@@ -1,9 +0,0 @@
-import { useZeroRocicorp } from "@package/database/client";
-
-/**
- * Returns the current Zero instance from context.
- * In Zero 0.25, useZero returns a signal, so we unwrap it with ()()
- */
-export const useZeroInstance = () => {
-	return useZeroRocicorp()();
-};
