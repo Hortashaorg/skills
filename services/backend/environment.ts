9037dcbb0ebee6db35c09ada206772d14f582c5f
Hortasha <eidemartin_303@hotmail.com>
2025-12-15 18:26:12 +0000

    introduce getters and remove dead file

--- services/backend/environment.ts
+++ services/backend/environment.ts
@@ -2,35 +2,55 @@ import { throwError } from "@package/common";
 
 export const environment = {
 	// Zitadel OIDC Configuration
-	ZITADEL_ISSUER:
-		process.env.ZITADEL_ISSUER ??
-		throwError("Env Variable: ZITADEL_ISSUER not found"),
-	ZITADEL_CLIENT_ID:
-		process.env.ZITADEL_CLIENT_ID ??
-		throwError("Env Variable: ZITADEL_CLIENT_ID not found"),
-	ZITADEL_CLIENT_SECRET:
-		process.env.ZITADEL_CLIENT_SECRET ??
-		throwError("Env Variable: ZITADEL_CLIENT_SECRET not found"),
-
+	get ZITADEL_ISSUER() {
+		return (
+			process.env.ZITADEL_ISSUER ??
+			throwError("Env Variable: ZITADEL_ISSUER not found")
+		);
+	},
+	get ZITADEL_CLIENT_ID() {
+		return (
+			process.env.ZITADEL_CLIENT_ID ??
+			throwError("Env Variable: ZITADEL_CLIENT_ID not found")
+		);
+	},
+	get ZITADEL_CLIENT_SECRET() {
+		return (
+			process.env.ZITADEL_CLIENT_SECRET ??
+			throwError("Env Variable: ZITADEL_CLIENT_SECRET not found")
+		);
+	},
 	// Application Configuration
-	NODE_ENV:
-		process.env.NODE_ENV ?? throwError("Env Variable: NODE_ENV not found"),
-	FRONTEND_URL:
-		process.env.FRONTEND_URL ??
-		throwError("Env Variable: FRONTEND_URL not found"),
-
+	get NODE_ENV() {
+		return (
+			process.env.NODE_ENV ?? throwError("Env Variable: NODE_ENV not found")
+		);
+	},
+	get FRONTEND_URL() {
+		return (
+			process.env.FRONTEND_URL ??
+			throwError("Env Variable: FRONTEND_URL not found")
+		);
+	},
 	// Zero Cache base url
-	CACHE_BASE_URL:
-		process.env.CACHE_BASE_URL ??
-		throwError("Env Variable: CACHE_BASE_URL not found"),
-
+	get CACHE_BASE_URL() {
+		return (
+			process.env.CACHE_BASE_URL ??
+			throwError("Env Variable: CACHE_BASE_URL not found")
+		);
+	},
 	// Database Configuration
-	ZERO_UPSTREAM_DB:
-		process.env.ZERO_UPSTREAM_DB ??
-		throwError("Env Variable: ZERO_UPSTREAM_DB not found"),
-
+	get ZERO_UPSTREAM_DB() {
+		return (
+			process.env.ZERO_UPSTREAM_DB ??
+			throwError("Env Variable: ZERO_UPSTREAM_DB not found")
+		);
+	},
 	// JWT Configuration
-	AUTH_PRIVATE_KEY:
-		process.env.AUTH_PRIVATE_KEY ??
-		throwError("Env Variable: AUTH_PRIVATE_KEY not found"),
+	get AUTH_PRIVATE_KEY() {
+		return (
+			process.env.AUTH_PRIVATE_KEY ??
+			throwError("Env Variable: AUTH_PRIVATE_KEY not found")
+		);
+	},
 } as const;
